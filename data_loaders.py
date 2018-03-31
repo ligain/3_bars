b19d049d103d411e172d29f6127e2f562b6e3ee6
ligain <khokhlov86@gmail.com>
2018-03-31 22:21:36 +0300

    changed variable name in data_loaders.py

--- data_loaders.py
+++ data_loaders.py
@@ -1,19 +1,17 @@
 import json
-import os
 
 
 def load_from_json(filepath):
     status = False
 
     try:
-        file = open(filepath, 'r')
+        json_file = open(filepath, 'r')
     except FileNotFoundError as err:
         return status, str(err)
 
     try:
-        result = json.load(file)
+        result = json.load(json_file)
     except json.decoder.JSONDecodeError as err:
         return status, str(err)
 
     return True, result
-
