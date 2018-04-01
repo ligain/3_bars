44fbc6d313c8f1ec7339d119b8e60075d06632d8
ligain <khokhlov86@gmail.com>
2018-04-01 21:14:19 +0300

    Added some items in README Changed names of some variables longitude and latitude now required params

--- bars.py
+++ bars.py
@@ -13,32 +13,23 @@ def calc_geo_cords_delta(user_longitude, user_latitude, bar):
     )
 
 
-def input_user_cords():
-    raw_input = input("Введите координаты, разделенные запятой "
-                      "в формате: долгота, широта\n"
-                      " например: 37.621, 55.76536 -> ")
-    cords = list(map(float, raw_input.split(',')))
-    return cords
-
-
 def load_data(filepath):
     with open(filepath, 'r') as json_file:
-        output = json.load(json_file)
-    return output
+        decoded_data = json.load(json_file)
+    return decoded_data
 
 
-def get_biggest_bar(bars_data):
-    bars = bars_data.get("features")
+def get_biggest_bar(bars):
     return max(bars, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
-def get_smallest_bar(bars_data):
-    bars = bars_data.get("features")
+def get_smallest_bar(bars):
     return min(bars, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
-def get_closest_bar(bars_data, longitude, latitude):
-    bars = bars_data.get("features")
+def get_closest_bar(bars, longitude, latitude):
+    # generate and sort a list of tuples with coordinates differences like:
+    # [(0.45, 0.5656, {...}), (0.0012, 0.322, {...}), (0.985, 0.124554, {...}), ...]
     deltas = list(min(calc_geo_cords_delta(longitude, latitude, bar) for bar in bars))
     closest_bar = deltas[2]
     return closest_bar
@@ -47,41 +38,40 @@ def get_closest_bar(bars_data, longitude, latitude):
 def get_args():
     parser = argparse.ArgumentParser(
         description='Скрипт расчета информации о московских барах.')
-    parser.add_argument("-f", "--filename",
-                        help="Путь к файлу с информацией "
-                             "о барах в формате json",
+    parser.add_argument('-f', '--filepath',
+                        help='Путь к файлу с информацией '
+                             'о барах в формате json',
                         default='bars.json')
-    parser.add_argument("--longitude",
-                        help="Долгота вашего местонахождения",
-                        type=float)
-    parser.add_argument("--latitude",
-                        help="Широта вашего местонахождения",
-                        type=float)
-    args = parser.parse_args()
-    return args.filename, args.longitude, args.latitude
+    parser.add_argument('--longitude',
+                        help='Долгота вашего местонахождения',
+                        type=float,
+                        required=True)
+    parser.add_argument('--latitude',
+                        help='Широта вашего местонахождения',
+                        type=float,
+                        required=True)
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    bars_filepath, user_longitude, user_latitude = get_args()
+    args = get_args()
 
-    bars_data = load_data(bars_filepath)
+    bars_raw_data = load_data(args.filepath)
 
-    if not bars_data:
-        print('Отсутствует информация о барах')
-        exit()
+    if not bars_raw_data:
+        exit('Отсутствует информация о барах')
 
-    biggest = get_biggest_bar(bars_data)
+    bars_data = bars_raw_data.get('features')
+
+    biggest_bar = get_biggest_bar(bars_data)
     print('Самый большой бар: {bar[properties][Attributes][Name]} '
-          'с: {bar[properties][Attributes][SeatsCount]} мест'.format(bar=biggest))
+          'с: {bar[properties][Attributes][SeatsCount]} мест'.format(bar=biggest_bar))
 
-    smallest = get_smallest_bar(bars_data)
+    smallest_bar = get_smallest_bar(bars_data)
     print('Наименьший бар: {bar[properties][Attributes][Name]} '
-          'с: {bar[properties][Attributes][SeatsCount]} мест'.format(bar=smallest))
+          'с: {bar[properties][Attributes][SeatsCount]} мест'.format(bar=smallest_bar))
 
-    if not user_longitude or not user_latitude:
-        user_longitude, user_latitude = input_user_cords()
-
-    closest = get_closest_bar(bars_data, user_longitude, user_latitude)
+    closest_bar = get_closest_bar(bars_data, args.longitude, args.latitude)
     print('Ближайший бар: {bar[properties][Attributes][Name]} '
           'с долготой: {bar[geometry][coordinates][0]} '
-          'и широтой: {bar[geometry][coordinates][1]}'.format(bar=closest))
+          'и широтой: {bar[geometry][coordinates][1]}'.format(bar=closest_bar))
